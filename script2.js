45dc66cf2abebd7fe7e40850a60f11f6348ceb6d
Artem <comfort.stud@gmail.com>
2021-03-30 22:29:50 +0300

    praktika chast 4 Objects

--- script2.js
+++ script2.js
@@ -1,25 +1,2 @@
 "use strict";
 
-const str = "teSt";
-
-// console.log(str[2] = 'd');
-
-// console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
-
-const fruit = "Some fruit";
-
-console.log(fruit.indexOf("q"));
-
-const logg = "Hello world";
-
-console.log(logg.slice(6, 11));
-
-console.log(logg.substring(6, 11));
-
-console.log(logg.substr(6, 11));
-
-const test = "12.6px";
-
-console.log(parseInt(test));
